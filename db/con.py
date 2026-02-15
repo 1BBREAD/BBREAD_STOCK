392207ac6ea6682e0881e7c78828bf0a5c6b9e23
1BBREAD <147069356+1BBREAD@users.noreply.github.com>
2026-02-16 01:10:01 +0900

    Update con.py

--- db/con.py
+++ db/con.py
@@ -2,7 +2,7 @@ import streamlit as st
 from supabase import create_client, Client
 
 @st.cache_resource
-def db_con() :
+def con() :
   url = st.secrets["SUPABASE_URL"]
   key = st.secrets["SUPABASE_KEY"]
   supabase = create_client(url,key)
