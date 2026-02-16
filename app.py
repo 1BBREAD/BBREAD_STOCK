5e0b83145d1a5c58975c54d7ba97d9881dd3e3d1
1BBREAD <147069356+1BBREAD@users.noreply.github.com>
2026-02-16 11:33:01 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,96 +4,9 @@ import screening.growth as growth
 import inqr.corpNameCodeInqr as ic
 from db.con import con
 
+
 st.set_page_config(page_title="Value Screener", layout="wide")
 
-st.title("📊 가치투자 스크리너 (Local MVP)")
+st.title("📊 가치투자 스크리너")
 
-revenue_growth = st.slider("매출 성장률(%)", 3, 20, 10)
-roe_limit = st.slider("ROE 최소 (%)", 0, 30, 10)
-
-corp_name = st.text_input("기업명")
-
-year = st.number_input(
-    "특정 연도",
-    min_value=2000,
-    max_value=2100,
-    step=1
-)
-
-quarter = st.selectbox(
-    "특정 분기",
-    options=[None, 1, 2, 3, 4],
-    format_func=lambda x: "선택 안함" if x is None else f"{x}분기"
-)
-
-client_db = con()
-corp_code = ''
-
-if corp_name.strip():
-    corp_code = ic.get_corp_code_by_name(client_db, corp_name.strip())
-
-if st.button("YoY 전년도 결산 대비") :
-    # 1번 쿼리 : 특정 연도만 필요 
-    result1 = growth.get_revenue_growth_yoy(client_db, year, revenue_growth, corp_code)
-    if result1 :
-        st.write("YoY 결산 증가 데이터", result1)
-        data = result1
-        df = pd.DataFrame(data)
-        df["growth"] = df["growth"].astype(float)
-    
-        df = df.rename(columns={
-        "corp_name": "기업명",
-        "re_cur": "당기 매출",
-        "re_base": "기준 매출",
-        "growth": "증가율(%)"
-        })
-    
-        st.dataframe(df, use_container_width=True,
-        column_config={
-        "당기 매출": st.column_config.NumberColumn(format="%,d"),
-        "기준 매출": st.column_config.NumberColumn(format="%,d"),
-        "증가율(%)": st.column_config.NumberColumn(format="%.2f")
-        })
-if st.button("YoY 전분기 대비") :
-    result2 = growth.get_revenue_growth_yoy_quarter(client_db, year,revenue_growth, corp_code, quarter)
-
-    if result2 :
-        st.write(f"YoY {quarter}분기 전기 대비 증가 데이터")
-        data = result2
-        df = pd.DataFrame(data)
-        df["growth"] = df["growth"].astype(float)
-    
-        df = df.rename(columns={
-        "corp_name": "기업명",
-        "re_cur": "당기 매출",
-        "re_base": "기준 매출",
-        "growth": "증가율(%)"
-        })
-    
-        st.dataframe(df, use_container_width=True,
-        column_config={
-        "당기 매출": st.column_config.NumberColumn(format="%,d"),
-        "기준 매출": st.column_config.NumberColumn(format="%,d"),
-        "증가율(%)": st.column_config.NumberColumn(format="%.2f")
-        })
-if st.button("QoQ 전분기 대비"):
-    result3 = growth.get_revenue_growth_qoq(client_db, year,revenue_growth, corp_code, quarter) 
-    if result3 :
-        st.write(f"QoQ {quarter}분기 전분기 대비 증가 데이터")
-        data = result3
-        df = pd.DataFrame(data)
-        df["growth"] = df["growth"].astype(float)
-    
-        df = df.rename(columns={
-        "corp_name": "기업명",
-        "re_cur": "당기 매출",
-        "re_base": "기준 매출",
-        "growth": "증가율(%)"
-        })
-    
-        st.dataframe(df, use_container_width=True,
-        column_config={
-        "당기 매출": st.column_config.NumberColumn(format="%,d"),
-        "기준 매출": st.column_config.NumberColumn(format="%,d"),
-        "증가율(%)": st.column_config.NumberColumn(format="%.2f")
-        })
+st.write("왼쪽 사이드바에서 기능을 선택하세요.")
